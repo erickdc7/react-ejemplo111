45a2a28ee0dff56b84a0550d5bfe0f0040a1810f
ErickDC <erickdiazc717@gmail.com>
2023-01-19 17:56:50 -0500

    change 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First Application
+          First Application - Change 1
         </p>
         <a
           className="App-link"
